0e081eb0a9bbf1364a6f5e3efdb1c010f6a131d6
mahiya47 <ssmahiya7@gmail.com>
2026-06-18 22:52:03 +0530

    api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.4:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://192.168.1.4:5000/api",
 });
 
 api.interceptors.request.use((config) => {
