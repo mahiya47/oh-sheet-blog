5450dca1e10f1091b0b6ccdc9b56c3d084a1c987
mahiya47 <ssmahiya7@gmail.com>
2026-06-25 13:26:31 +0530

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext.jsx';
-import { ToastProvider } from './context/ToastContext.jsx';
-import { StoreProvider } from './lib/store.jsx';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ToastProvider } from "./context/ToastContext.jsx";
+import { StoreProvider } from "./lib/store.jsx";
+import { ChatProvider } from "./context/ChatContext.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <ToastProvider>
         <StoreProvider>
-          {/* basename keeps routing correct if you deploy under a subpath (GitHub Pages) */}
-          <BrowserRouter basename={import.meta.env.BASE_URL}>
-            <App />
-          </BrowserRouter>
+          <ChatProvider>
+            <BrowserRouter basename={import.meta.env.BASE_URL}>
+              <App />
+            </BrowserRouter>
+          </ChatProvider>
         </StoreProvider>
       </ToastProvider>
     </ThemeProvider>
