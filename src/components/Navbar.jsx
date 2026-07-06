8bf1b163b4edd9e552fb4db2ec3755d0b3120512
mahiya47 <ssmahiya7@gmail.com>
2026-07-06 10:12:54 +0530

    navabr fix for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -84,12 +84,11 @@ export default function Navbar() {
         >
           <Menu size={22} />
         </button>
-        <Link
-          to="/feed"
-          className="nav-logo wordmark"
-          aria-label="Oh Sheet home"
-        >
-          Oh <span>sheet!</span>
+        <Link to="/feed" className="nav-logo" aria-label="Oh Sheet home">
+          <span className="wordmark-desktop wordmark">
+            Oh <span>sheet!</span>
+          </span>
+          <img src="/favicon.svg" alt="Oh sheet!" className="wordmark-mobile" />
         </Link>
       </div>
 
