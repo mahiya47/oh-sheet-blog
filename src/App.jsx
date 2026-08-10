ad798d1e9810af7c61b6ab3740f0fb129332b7ea
mahiya47 <ssmahiya7@gmail.com>
2026-08-10 12:50:32 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,10 @@ import OnboardingPage from "./pages/OnboardingPage.jsx";
 import AdminPage from "./pages/AdminPage.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
 
+// 👇 ADDED IMPORTS FOR YOUR NEW PAGES
+import GamingPage from "./pages/GamingPage.jsx";
+import SnakePage from "./pages/SnakePage.jsx";
+
 export default function App() {
   return (
     <>
@@ -65,6 +69,11 @@ export default function App() {
               element={<StaticPage page="agreement" />}
             />
             <Route path="/admin" element={<AdminPage />} />
+
+            {/* 👇 ADDED YOUR ARCADE ROUTES HERE */}
+            <Route path="/games" element={<GamingPage />} />
+            <Route path="/snake" element={<SnakePage />} />
+
             <Route path="*" element={<NotFound />} />
           </Route>
         </Route>
